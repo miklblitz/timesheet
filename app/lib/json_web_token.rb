6606f21b2316a6ab901ec4b4706fdbb9dfc708b3
MIchael <miklblitz@yandex.ru>
2020-06-23 04:47:39 +0400

    have added errors

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -19,3 +19,11 @@ class JsonWebToken
     raise ExceptionHandler::InvalidToken, e.message
   end
 end
+
+
+module ExceptionHandler
+  extend ActiveSupport::Concern
+  class AuthenticationError < StandardError; end
+  class MissingToken < StandardError; end
+  class InvalidToken < StandardError; end
+end
