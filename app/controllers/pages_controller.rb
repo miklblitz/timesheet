d9686ecb599eb5e00fd34f8147e211d05d65688b
MIchael <miklblitz@yandex.ru>
2020-06-23 05:04:49 +0400

    unauth acces to pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@
 
 # An Pages Controller
 class PagesController < ApplicationController
-
+  skip_before_action :authorize_request
+  
   def index
     json_response(
       message: 'You are welcome to API REST service',
